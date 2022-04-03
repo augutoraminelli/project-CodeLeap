fd883fa18345721b371eac7daa837b4eb7592243
Augusto <augusto.raminelli>
2022-04-03 18:13:32 -0300

    create postCard css file

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../styles/postCard.css'
 
 function PostCard({ post, i }) {
 
   const { title, content } = post;
   console.log(post);
     return (
-      <div>
+      <div className='ctn-postcard'>
         <h2 className='header-postcard'>{ title }</h2>
         <p className='content card'>{ content }</p>
       </div>
